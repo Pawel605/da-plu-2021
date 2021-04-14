a44a044bf210727822ca10ffdabd880b8626151b
Paweł F <pawel605@gmail.com>
2021-04-14 20:57:24 +0200

    Add tests

---
+++ tests.py
@@ -0,0 +1,43 @@
+from fastapi.testclient import TestClient
+
+from main import app
+import pytest
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
+	
+
+def test_hello_name():
+    name = 'Kamila'
+    response = client.get(f"/hello/{name}")
+    assert response.status_code == 200
+    assert response.text == f'"Hello {name}"'
+
+
+@pytest.mark.parametrize("name", ["Zenek", "Marek", "Alojzy Niezdąży"])
+def test_hello_name(name):
+    response = client.get(f"/hello/{name}")
+    assert response.status_code == 200
+    assert response.text == f'"Hello {name}"'
+
+
+def test_counter():
+    response = client.get(f"/counter")
+    assert response.status_code == 200
+    assert response.text == "1"
+    # 2nd Try
+    response = client.get(f"/counter")
+    assert response.status_code == 200
+    assert response.text == "2"
+	 # 3th Try
+    response = client.get(f"/counter")
+    assert response.status_code == 200
+    assert response.text == "3"
+
+
+
