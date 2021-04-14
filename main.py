545681454490f197612adde04458411f77e31fa3
Paweł F <pawel605@gmail.com>
2021-04-14 18:54:56 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+app.counter = 0
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+@app.get("/hello/{name}")
+async def hello_name_view(name: str):
+    return f"Hello {name}"
+
+@app.get('/counter')
+def counter():
+    app.counter += 1
+    return app.counter
