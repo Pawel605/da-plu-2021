d228ff567d36b93f6e0f94421de48dce7c2a8565
Paweł F <pawel605@gmail.com>
2021-04-20 23:59:40 +0200

    task 1.1

--- main.py
+++ main.py
@@ -32,63 +32,63 @@ async def read_item(name: str):
 def root():
     return {"message": "Hello world!"}
 
-
-# task 1.2
-@app.get("/method", status_code=200)
-@app.post("/method", status_code=201)
-@app.delete("/method", status_code=200)
-@app.put("/method", status_code=200)
-@app.options("/method", status_code=200)
-def read_request(request: Request):
-    return {"method": request.method}
-
-
-# task 1.3
-@app.get("/auth", status_code=401)
-def get_auth(password: Optional[str] = None, password_hash: Optional[str] = None):
-    if not password or not password_hash or hashlib.sha512(str(password).encode("utf-8")).hexdigest() != str(
-            password_hash):
-        return Response(status_code=401)
-    else:
-        return Response(status_code=204)
-
-
-# task 1.4
-app.patient_id = 1
-app.patients = {}
-
-
-class PatientIn(BaseModel):
-    name: str
-    surname: str
-
-
-class PatientOut(BaseModel):
-    id: int
-    name: str
-    surname: str
-    register_date: date
-    vaccination_date: date
-
-
-@app.post("/register", status_code=201)
-def post_register(patient: PatientIn):
-    patient_id = app.patient_id
-    app.patient_id += 1
-    tmpDate = date.today()
-    register_date = tmpDate.strftime("%Y-%m-%d")
-    length_name_plus_surname = len([letter for letter in patient.name + patient.surname if letter.isalpha()])
-    vaccination_date = (tmpDate + timedelta(days=length_name_plus_surname)).strftime("%Y-%m-%d")
-
-    patient_out = PatientOut(
-        id=patient_id,
-        name=patient.name,
-        surname=patient.surname,
-        register_date=register_date,
-        vaccination_date=vaccination_date
-    )
-    app.patients[patient_id] = patient_out
-    return patient_out
+#
+# # task 1.2
+# @app.get("/method", status_code=200)
+# @app.post("/method", status_code=201)
+# @app.delete("/method", status_code=200)
+# @app.put("/method", status_code=200)
+# @app.options("/method", status_code=200)
+# def read_request(request: Request):
+#     return {"method": request.method}
+#
+#
+# # task 1.3
+# @app.get("/auth", status_code=401)
+# def get_auth(password: Optional[str] = None, password_hash: Optional[str] = None):
+#     if not password or not password_hash or hashlib.sha512(str(password).encode("utf-8")).hexdigest() != str(
+#             password_hash):
+#         return Response(status_code=401)
+#     else:
+#         return Response(status_code=204)
+#
+#
+# # task 1.4
+# app.patient_id = 1
+# app.patients = {}
+#
+#
+# class PatientIn(BaseModel):
+#     name: str
+#     surname: str
+#
+#
+# class PatientOut(BaseModel):
+#     id: int
+#     name: str
+#     surname: str
+#     register_date: date
+#     vaccination_date: date
+#
+#
+# @app.post("/register", status_code=201)
+# def post_register(patient: PatientIn):
+#     patient_id = app.patient_id
+#     app.patient_id += 1
+#     tmpDate = date.today()
+#     register_date = tmpDate.strftime("%Y-%m-%d")
+#     length_name_plus_surname = len([letter for letter in patient.name + patient.surname if letter.isalpha()])
+#     vaccination_date = (tmpDate + timedelta(days=length_name_plus_surname)).strftime("%Y-%m-%d")
+#
+#     patient_out = PatientOut(
+#         id=patient_id,
+#         name=patient.name,
+#         surname=patient.surname,
+#         register_date=register_date,
+#         vaccination_date=vaccination_date
+#     )
+#     app.patients[patient_id] = patient_out
+#     return patient_out
 
 
 # task 1.5
