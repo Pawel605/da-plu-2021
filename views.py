96daeaf606ff67dfa8554d619017dbe0e721e86c
Paweł F <pawel605@gmail.com>
2021-05-18 04:36:40 +0200

    Task 5.2

--- views.py
+++ views.py
@@ -24,14 +24,34 @@ async def get_shippers(db: Session = Depends(get_db)):
     return crud.get_shippers(db)
 
 
+# task 5.1
 @router.get("/suppliers/{supplier_id}", response_model=schemas.SupplierExtended)
 async def get_supplier(supplier_id: PositiveInt, db: Session = Depends(get_db)):
     db_supplier = crud.get_supplier(db, supplier_id)
     if db_supplier is None:
-        raise HTTPException(status_code=404,detail="Supplier not found")
+        raise HTTPException(status_code=404, detail="Supplier not found")
     return db_supplier
 
 
 @router.get("/suppliers", response_model=List[schemas.Supplier])
 async def get_suppliers(db: Session = Depends(get_db)):
     return crud.get_suppliers(db)
+
+
+# task 5.2
+@router.get("/suppliers/{supplier_id}/products")
+async def get_suppliers_products(supplier_id: PositiveInt, db: Session = Depends(get_db)):
+    db_supplier = crud.get_supplier(db, supplier_id)
+    if db_supplier is None:
+        raise HTTPException(status_code=404, detail="Supplier not found")
+
+    db_suppliers_products = crud.get_suppliers_products(db, supplier_id)
+    return [{
+        "ProductID": product.ProductID,
+        "ProductName": product.ProductName,
+        "Category": {
+            "CategoryID": product.CategoryID,
+            "CategoryName": product.CategoryName,
+        },
+        "Discontinued": product.Discontinued,
+    } for product in db_suppliers_products]
